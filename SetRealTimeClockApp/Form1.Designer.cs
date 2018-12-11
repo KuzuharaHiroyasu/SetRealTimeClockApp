7b24a3fa2f0d0a2661ffe4c4409121d6a2b95eed
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-12-11 14:02:43 +0900

    ベースコード

---
+++ SetRealTimeClockApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SetRealTimeClockApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.setClock_button = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // setClock_button
+            // 
+            this.setClock_button.Location = new System.Drawing.Point(73, 93);
+            this.setClock_button.Name = "setClock_button";
+            this.setClock_button.Size = new System.Drawing.Size(75, 23);
+            this.setClock_button.TabIndex = 1;
+            this.setClock_button.Text = "時計セット";
+            this.setClock_button.UseVisualStyleBackColor = true;
+            this.setClock_button.Click += new System.EventHandler(this.setClock_button_Click);
+            // 
+            // serialPort
+            // 
+            this.serialPort.DtrEnable = true;
+            this.serialPort.PortName = "COM5";
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(42, 37);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 12);
+            this.label.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 127);
+            this.ControlBox = false;
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.setClock_button);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "時刻設定";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button setClock_button;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ SetRealTimeClockApp/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetRealTimeClockApp
+{
+    public struct SystemTime
+    {
+        public ushort wYear;
+        public ushort wMonth;
+        public ushort wDayOfWeek;
+        public ushort wDay;
+        public ushort wHour;
+        public ushort wMinute;
+        public ushort wSecond;
+        public ushort wMiliseconds;
+    }
+
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool SetLocalTime(ref SystemTime sysTime);
+
+        SystemTime sysTime = new SystemTime();
+        string clock = "";
+//        string temp = "";
+        bool ret = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        /************************************************************************/
+        /* 関数名   : Form1_Shown          						    			*/
+        /* 機能     : フォーム表示時イベント		                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            serialPort.PortName = "COM5";   // 選択されたCOMをポート名に設定
+            serialPort.Open();  // ポートを開く
+            
+            Task.Run(() => writeData());    // 別タスクで時刻設定コマンド送信
+        }
+
+        /************************************************************************/
+        /* 関数名   : writeData          						    			*/
+        /* 機能     : 時刻設定コマンド送信  		                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
+        private void writeData()
+        {
+            byte[] param = new byte[1];
+            param[0] = (byte)2; // 時刻設定コード
+            do
+            {
+                serialPort.Write(param, 0, param.Length);
+                Task.Delay(1000);
+            } while (!ret);
+            endProcessing();
+        }
+
+        /************************************************************************/
+        /* 関数名   : setClock_button_Click         						    */
+        /* 機能     : ボタンクリック時イベント		                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
+        private void setClock_button_Click(object sender, EventArgs e)
+        {
+        }
+
+        /************************************************************************/
+        /* 関数名   : serialPort_DataReceived    				    			*/
+        /* 機能     : Arduinoからのデータ受信関数	                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort.ReadExisting();	// ポートから文字列を受信する
+                if (!string.IsNullOrEmpty(data))
+                {
+                    Invoke((MethodInvoker)(() =>	// 受信用スレッドから切り替えてデータを書き込む
+                    {
+                        switch(data)
+                        {
+                            case "YEAR":
+                                sysTime.wYear = Convert.ToUInt16(clock);
+                                break;
+                            case "MONTH":
+                                sysTime.wMonth = Convert.ToUInt16(clock);
+                                break;
+                            case "DAY":
+                                sysTime.wDay = Convert.ToUInt16(clock);
+                                break;
+                            case "HOUR":
+                                sysTime.wHour = Convert.ToUInt16(clock);
+                                break;
+                            case "MINUTE":
+                                sysTime.wMinute = Convert.ToUInt16(clock);
+                                break;
+                            case "SECOND":
+                                sysTime.wSecond = Convert.ToUInt16(clock);
+                                sysTime.wMiliseconds = 0;
+                                break;
+                            case "END":
+                                // システム時刻を設定する
+                                ret = SetLocalTime(ref sysTime);
+//                                label.Text = temp;
+                                break;
+                            default:
+                                clock = data;
+//                                temp = temp + data;
+                                break;
+                        }
+                        Thread.Sleep(10);
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /************************************************************************/
+        /* 関数名   : endProcessing          					    			*/
+        /* 機能     : 終了処理                		                            */
+        /* 引数     : なし                                                      */
+        /* 戻り値   : なし														*/
+        /************************************************************************/
+        private void endProcessing()
+        {
+            serialPort.Close();
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start("C:\\PCAnalysisApp\\SleepCheckerApp.exe");
+            this.Close();
+        }
+    }
+}
